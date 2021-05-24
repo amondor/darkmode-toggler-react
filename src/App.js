be56e8ef4de8edcf9493ba9293b4d0d8f27bd857
Andrew Mondor <mondor.andrew@gmail.com>
2021-05-24 21:47:39 +0200

    creating switch and inner box

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import "./styles/output.css";
+import React, { useState } from "react";
+import "./styles/App.css";
 
-function App() {
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
-      <h1 className="text-5xl text-white">Hello Tailwind 👋</h1>
-      <p className="text-gray-400 mt-5 text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-        consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
-        eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
-      </p>
-      <button class="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
-        Hello Friends 🚀
-      </button>
+    <div
+      class={`h-screen w-full flex items-center justify-center bg-blue-600 flex-col ${
+        darkMode && "dark"
+      }`}
+    >
+      <label class="switch">
+        <input type="checkbox" onClick={() => setDarkMode(!darkMode)} />
+        <span class="slider round"></span>
+      </label>
+      <div className="h-5/12 w-5/12 bg-gray-200 dark:bg-gray-900 p-5 rounded-lg mt-4 text-white">
+        <h1 className="text-gray-800 dark:text-gray-200 font-extrabold text-3xl">
+          Hello Dark Mode 👋
+        </h1>
+        <p className="text-gray-800 dark:text-gray-200 font-light mt-3 text-sm">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit itaque
+          dolorem officia beatae minus totam. Earum amet deserunt numquam
+          molestias veniam excepturi dicta obcaecati
+        </p>
+        <button className="bg-white dark:bg-gray-800 py-2 px-3 font-bold text-blue-800 dark:text-white mt-3 rounded-md border border-gray-400 dark:border-gray-600">
+          Button
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
-
